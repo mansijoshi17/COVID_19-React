7c53500834d05d99a93bc596a56cc42c1b0dc93a
Mansi Joshi <mansijoshi1777@gmail.com>
2020-04-02 18:25:28 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
          Hello
+         <h1>dsds</h1>
     </div>
   );
 }
